aa5c1719959024e51dea869d3494db5d4cf69e28
PulkitGG <pulkit063@gmail.com>
2021-12-27 18:24:42 +0100

    web app to display upcoming movie list

---
+++ UpcomingMoviesWebApp/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.IO;
+using System.Net.TMDb;
+using UpcomingMoviesWebApp.Models;
+
+namespace UpcomingMoviesWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<ActionResult> IndexAsync(CancellationToken cancellationToken)
+        {
+            //var client = new HttpClient();
+            //var response = client.GetAsync("https://api.themoviedb.org/4/list/1?api_key=1f54bd990f1cdfb230adb312546d765d&page=1");
+            //var products = response.Content.ReadAsAsync<IEnumerable<MovieViewModel>>().Result;
+            MovieViewModel mv;
+            List<MovieViewModel> mvList = new List<MovieViewModel>();
+
+            using (var client = new ServiceClient("1f54bd990f1cdfb230adb312546d765d"))
+            {
+                for (int i = 1, count = 1000; i <= count; i++)
+                {
+                    var movies = await client.Movies.GetUpcomingAsync(null, i, cancellationToken);
+                    count = 2; // keep track of the actual page count
+
+                    foreach (Movie m in movies.Results)
+                    {
+                        mv = new MovieViewModel();
+                        var movie = await client.Movies.GetAsync(m.Id, null, true, cancellationToken);
+
+                        mv.MovieName = movie.OriginalTitle;
+                        mv.ImagePath = "http://image.tmdb.org/t/p/w500" + movie.Poster;
+                        mv.ReleaseDate = movie.ReleaseDate;
+                        mv.Description = movie.Overview.Replace(",", "").Replace("'", "");
+                        mv.Votes = movie.VoteCount;
+                        mv.Rating = movie.VoteAverage;
+
+
+                        List<string> genrelist = new List<string>();
+
+                        foreach (var genre in movie.Genres)
+                        {
+                            genrelist.Add(genre.Name);
+                        }
+
+                        mv.Genre = string.Join("/", genrelist);
+                        mvList.Add(mv);
+                    }
+                }
+            }
+
+            return View(mvList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UpcomingMoviesWebApp/Models/MovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace UpcomingMoviesWebApp.Models
+{
+    public class MovieViewModel
+    {
+        public string MovieName { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public int Votes { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
